357515c109206327efb09606596b268601f07532
user <user@DESKTOP-37GULAI>
2018-06-22 16:44:22 +0900

    h

---
+++ CommuProject/src/bean/MemberDTO.java
@@ -0,0 +1,49 @@
+package bean;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String name;
+	private String pay;
+	private String free;
+	private String exp;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPay() {
+		return pay;
+	}
+	public void setPay(String pay) {
+		this.pay = pay;
+	}
+	public String getFree() {
+		return free;
+	}
+	public void setFree(String free) {
+		this.free = free;
+	}
+	public String getExp() {
+		return exp;
+	}
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+	
+	
+}
