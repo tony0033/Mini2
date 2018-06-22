b2505b1e505f7f70b27b3dccd8016b5dc3bf7623
이승재 <@LAPTOP-21TFDD8C>
2018-06-22 16:50:06 +0900

    lee

---
+++ CommuProject/src/bean/BBSDTO.java
@@ -0,0 +1,47 @@
+package bean;
+
+public class BBSDTO {
+	private String title;
+	private String content;
+	private String writer;
+	private String date;
+	private String count;
+	private String blike;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	public String getBlike() {
+		return blike;
+	}
+	public void setBlike(String blike) {
+		this.blike = blike;
+	}
+}
---
+++ CommuProject/src/bean/TradeDTO.java
@@ -0,0 +1,34 @@
+package bean;
+
+public class TradeDTO {
+	private String name;
+	private String price;
+	private String writer;
+	private String count;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	
+	
+}
